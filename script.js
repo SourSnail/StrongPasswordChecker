dc5f7fe1bb621326248062808ceaa49a9fd7c7fc
SourSnail <99859845+SourSnail@users.noreply.github.com>
2024-02-13 17:37:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function checkPassword() {
+    const passwordInput = document.getElementById("password").value;
+    const errorElement = document.getElementById("error");
+
+    if (passwordInput.length < 8) {
+        showError("Password must be 8 characters or more.");
+    } else if (!/\d/.test(passwordInput)) {
+        showError("Password must include at least one number.");
+    } else if (!/[A-Z]/.test(passwordInput)) {
+        showError("Password must include at least one capital letter.");
+    } else {
+        errorElement.innerText = "";
+        alert(`Strong password detected. Password set to ${passwordInput}`);
+    }
+}
+
+function showError(message) {
+    const errorElement = document.getElementById("error");
+    errorElement.innerText = message;
+}
